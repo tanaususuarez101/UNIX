84022d822ad84ba4ff13128960885f60da6030d4
Tana - HP <tanausu.suarez101@alu.ulpgc.es>
2017-12-12 21:12:51 +0000

    Nuevo añadido en compare_snapshot

--- Practica_6/compare_snapshot.sh
+++ Practica_6/compare_snapshot.sh
@@ -9,23 +9,36 @@ find $rutas -type f -printf '%m ' -exec md5sum {} \; > /snapshot/registro_temp.t
 # + Archivos añadidos
 # - Archivos eliminados
 # El grep filtra y obtiene todas la coincidencias que empieze por alguno de esos símbolos
+perm_prov='x'
+cont_prev='x'
 
-del=$()
 
 diff -c /snapshot/registro.txt /snapshot/registro_temp.txt | grep -e "^[!+-] " |
 while read opc permisos md5 archivo
 do
-	
 	case "$opc" in	
 		"+")
 			echo "Archivo añadido :: $archivo ";;
 		"-")
 			echo "Archivo borrado :: $archivo";;
 		"!")
-			echo "Archivo cambiado :: $archivo";;
+			if [[ $perm_prov == 'x' ]]
+			then
+					perm_prov=$permisos;
+					cont_prov=$md5;
+			else
+				echo "Archivo modificado :: $archivo";
+				[[ $perm_prov != $permisos ]] && echo "-> Han cambiado los permisos de $perm_prov a $permisos";
+				[[ $cont_prov != $md5 ]] && echo "-> Ha cambiado el contenido";
+				perm_prov='x'
+				cont_prov='x'
+			fi
+			;;
 	esac
-done
 
+
+done
+echo "------------------"
 
 #Archivo añadido
 #Archivo borrado
